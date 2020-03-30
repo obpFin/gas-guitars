950d82ff5a61b131ef379d8cbb4273dab7d22463
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-03-30 20:09:31 +0300

    move server to src

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
-
-import http from "http";
-import express from "express";
+import http from 'http';
+import express from 'express';
+import { applyMiddleware } from './utils';
+import middleware from './middleware';
 
 const router = express();
+applyMiddleware(middleware, router);
+
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
 
