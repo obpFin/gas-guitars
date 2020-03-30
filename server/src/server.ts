fc136919d2e71ef31e3f02f5c948bdf9cbf7dfbf
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-03-30 20:56:07 +0300

    add error handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
 import http from 'http';
 import express from 'express';
-import { applyMiddleware } from './utils';
+import { applyMiddleware, applyRoutes } from './utils';
+import routes from './services';
 import middleware from './middleware';
+
+process.on('uncaughtException', e => {
+  // TODO: replace with logger
+  console.log(e);
+  process.exit(1);
+});
+process.on('unhandledRejection', e => {
+  // TODO: replace with logger
+  console.log(e);
+  process.exit(1);
+});
 
 const router = express();
 applyMiddleware(middleware, router);
+applyRoutes(routes, router);
 
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
