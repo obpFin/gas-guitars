369dee5b686d39319ad68140e3d3b58052e7e90a
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-06-08 19:32:15 +0300

    fix eslint errors

--- server/src/middleware/common.ts
+++ server/src/middleware/common.ts
@@ -4,18 +4,18 @@ import helmet from 'helmet';
 import parser from 'body-parser';
 import compression from 'compression';
 
-export const handleCors = (router: Router) =>
+export const handleCors = (router: Router): Router =>
   router.use(cors({ credentials: true, origin: true }));
 
-export const handleBodyRequestParsing = (router: Router) => {
+export const handleBodyRequestParsing = (router: Router): void => {
   router.use(parser.urlencoded({ extended: true }));
   router.use(parser.json());
 };
 
-export const handleCompression = (router: Router) => {
+export const handleCompression = (router: Router): void => {
   router.use(compression());
 };
 
-export const handleHelmet = (router: Router) => {
+export const handleHelmet = (router: Router): void => {
   router.use(helmet());
 };
--- server/src/utils/ErrorHandler.ts
+++ server/src/utils/ErrorHandler.ts
@@ -1,11 +1,11 @@
 import { Response, NextFunction } from "express";
 import { HTTPClientError, HTTP404Error } from "../utils/httpErrors";
 
-export const notFoundError = () => {
+export const notFoundError = (): void => {
   throw new HTTP404Error("Method not found.");
 };
 
-export const clientError = (err: Error, res: Response, next: NextFunction) => {
+export const clientError = (err: Error, res: Response, next: NextFunction): void => {
   if (err instanceof HTTPClientError) {
     console.warn(err);
     res.status(err.statusCode).send(err.message);
@@ -14,7 +14,7 @@ export const clientError = (err: Error, res: Response, next: NextFunction) => {
   }
 };
 
-export const serverError = (err: Error, res: Response, next: NextFunction) => {
+export const serverError = (err: Error, res: Response, next: NextFunction): void => {
   console.error(err);
   if (process.env.NODE_ENV === "production") {
     res.status(500).send("Internal Server Error");
