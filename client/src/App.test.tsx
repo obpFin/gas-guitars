65b13e207b3f0c425ce049566049913c60413154
Oskari Peltonen <oskari.peltonen@outlook.com>
2020-06-27 11:53:01 +0300

    initial gardening

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -6,6 +6,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const linkElement = getByText(/Gas Guitars/i)
   expect(linkElement).toBeInTheDocument()
 })
